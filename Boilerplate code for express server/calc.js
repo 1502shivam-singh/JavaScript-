48e24ade940a36f9a224d5ffa249b3095a437917
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-07-18 23:34:20 +0530

    Added the js file
    This JS file contains the backend code written on the server

---
+++ Boilerplate code for express server/calc.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParse=require("body-parser")
+const app = express();
+
+app.use(bodyParse.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+  res.sendFile(__dirname +"/style.css");
+});
+
+app.post("/",function(req,res){
+var bmi=((Number)(req.body.weight)/((Number)(req.body.height)*(Number)(req.body.height)));
+var rounded = bmi.toFixed(1);
+
+//Used for debuging
+//alert(rounded);
+//alert(typeof(rounded));
+
+var bmir=parseFloat(rounded);
+//alert(typeof(bmir));  Debugging
+if(bmir<18.5){
+    res.send("Yout BMI is "+bmir+", so you are underweight.");
+}
+else if((bmir=>18.5) && (bmir<25)){res.send("Your BMI is "+bmir+", so you have a normal weight.");}
+else
+   { res.send("Your BMI is "+bmir+", so you are overweight.");}
+})
+
+app.listen("3000",function(){
+  console.log("Server running");
+});
