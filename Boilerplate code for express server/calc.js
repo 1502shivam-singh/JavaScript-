cfb61e249b65d598f6d7f5d4cd7ef1ef5c6dde95
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-07-20 23:25:40 +0530

    Added comments for reability

--- Boilerplate code for express server/calc.js
+++ Boilerplate code for express server/calc.js
@@ -11,21 +11,17 @@ app.get("/",function(req,res){
 });
 
 app.post("/",function(req,res){
+//BMI Calculation starts  
 var bmi=((Number)(req.body.weight)/((Number)(req.body.height)*(Number)(req.body.height)));
 var rounded = bmi.toFixed(1);
-
-//Used for debuging
-//alert(rounded);
-//alert(typeof(rounded));
-
 var bmir=parseFloat(rounded);
-//alert(typeof(bmir));  Debugging
 if(bmir<18.5){
     res.send("Yout BMI is "+bmir+", so you are underweight.");
 }
 else if((bmir=>18.5) && (bmir<25)){res.send("Your BMI is "+bmir+", so you have a normal weight.");}
 else
    { res.send("Your BMI is "+bmir+", so you are overweight.");}
+  //BMI Calculation end
 })
 
 app.listen("3000",function(){
